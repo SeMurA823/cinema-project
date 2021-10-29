2d335b764f87462d5a5926891d17f73d331dafd8
semura823 <muravevartem@outlook.com>
2021-10-29 17:52:35 +0400

    EnableJpaRepository

--- src/main/java/com/muravyev/cinema/CinemaApplication.java
+++ src/main/java/com/muravyev/cinema/CinemaApplication.java
@@ -2,8 +2,10 @@ package com.muravyev.cinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class CinemaApplication {
 
     public static void main(String[] args) {
