744d7bdcc20c8f599e62d80c7198d043f4e5f64e
semura823 <muravevartem@outlook.com>
2021-10-30 18:09:25 +0400

    add poster rest controller

---
+++ src/main/java/com/muravyev/cinema/controllers/rest/FilmPosterRestController.java
@@ -0,0 +1,22 @@
+package com.muravyev.cinema.controllers.rest;
+
+import com.muravyev.cinema.services.FilmPosterService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/poster")
+public class FilmPosterRestController {
+    private FilmPosterService posterService;
+
+    public FilmPosterRestController(FilmPosterService posterService) {
+        this.posterService = posterService;
+    }
+
+    @PostMapping("/{film}")
+    public ResponseEntity<?> upload(@RequestParam("file") MultipartFile file, @PathVariable("film") long filmId) {
+        String filename = posterService.save(file, filmId);
+        return ResponseEntity.ok(filename);
+    }
+}
