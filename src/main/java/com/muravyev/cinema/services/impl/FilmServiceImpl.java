6357a4d4500bbfa8be11caf5583cf2e03a109bfc
semura823 <muravevartem@outlook.com>
2021-10-30 18:08:50 +0400

    add logging

--- src/main/java/com/muravyev/cinema/services/impl/FilmServiceImpl.java
+++ src/main/java/com/muravyev/cinema/services/impl/FilmServiceImpl.java
@@ -3,14 +3,16 @@ package com.muravyev.cinema.services.impl;
 import com.muravyev.cinema.entities.film.Film;
 import com.muravyev.cinema.repo.FilmRepository;
 import com.muravyev.cinema.services.FilmService;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.Level;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
+@Log4j2
 public class FilmServiceImpl implements FilmService {
     private FilmRepository filmRepository;
 
@@ -20,11 +22,14 @@ public class FilmServiceImpl implements FilmService {
 
     @Override
     public Optional<Film> getFilm(Long id) {
+        log.log(Level.DEBUG, "Getting film with id = {}", id);
         return filmRepository.findById(id);
     }
 
     @Override
     public Page<Film> getPremieres(Pageable pageable) {
+        log.log(Level.DEBUG, "Pageable (premieres): {}", pageable);
         return filmRepository.comingPremieres(pageable);
     }
+
 }
