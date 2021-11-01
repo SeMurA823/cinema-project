33770937e595ca73438044059dd2ccd3e436523d
semura823 <muravevartem@outlook.com>
2021-11-02 01:19:07 +0400

    add basic authZ (temporarily)

--- src/main/java/com/muravyev/cinema/security/SecurityConfig.java
+++ src/main/java/com/muravyev/cinema/security/SecurityConfig.java
@@ -1,15 +1,26 @@
 package com.muravyev.cinema.security;
 
+import com.muravyev.cinema.entities.roles.Role;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public void setUserDetailsService(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @Bean
     public BCryptPasswordEncoder encoder() {
@@ -19,11 +30,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                    .disable()
-                .httpBasic()
-                    .disable()
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll();
+                .antMatchers("/api/users/registration").permitAll()
+                .antMatchers("/api/films/add", "/api/posters/*/add", "/api/films/*/delete").hasAuthority(Role.ADMIN.getAuthority())
+                .anyRequest().permitAll()
+                .and().httpBasic()
+                .and().sessionManagement().disable()
+                .userDetailsService(userDetailsService);
     }
 }
