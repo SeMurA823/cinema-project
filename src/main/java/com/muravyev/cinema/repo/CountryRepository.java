807792aed49a86a54e5dad1466f5e34b9012dcd6
semura823 <muravevartem@outlook.com>
2021-11-02 01:13:19 +0400

    add CountryRepository

---
+++ src/main/java/com/muravyev/cinema/repo/CountryRepository.java
@@ -0,0 +1,7 @@
+package com.muravyev.cinema.repo;
+
+import com.muravyev.cinema.entities.film.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, String> {
+}
